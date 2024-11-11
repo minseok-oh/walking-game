6843c3cca4e043e1ed8b11faf929ae30fcec5342
MinSeok Oh <minseok.dev@gmail.com>
2024-11-11 17:07:14 +0900

    setting: cors 해결을 위한 WebConfig 정의

---
+++ server/src/main/java/org/calorieburn/server/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.calorieburn.server.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true)
+                .exposedHeaders("Custom-Header")
+                .maxAge(3600);
+    }
+}
