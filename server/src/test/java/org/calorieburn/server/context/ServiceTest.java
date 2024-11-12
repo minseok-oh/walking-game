c26be316faf046b1db5088dedb83fbf7321442a3
MinSeok Oh <minseok.dev@gmail.com>
2024-11-12 15:24:38 +0900

    test: 로그인을 위한 테스트 코드 작성

--- server/src/test/java/org/calorieburn/server/context/ServiceTest.java
+++ server/src/test/java/org/calorieburn/server/context/ServiceTest.java
@@ -1,18 +1,22 @@
 package org.calorieburn.server.context;
 
 import org.calorieburn.server.core.member.infra.MemberCoreRepository;
-import org.calorieburn.server.core.member.service.MemberService;
+import org.calorieburn.server.core.member.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 public abstract class ServiceTest {
 
     @Autowired
+    protected PasswordEncoder passwordEncoder;
+
+    @Autowired
     protected MemberCoreRepository memberCoreRepository;
 
     @Autowired
-    protected MemberService memberService;
+    protected AuthService authService;
 
 }
